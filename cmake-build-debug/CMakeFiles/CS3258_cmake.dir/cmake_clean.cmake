8fa5373faa191197b4e89e83207e211884a36887
bobbybodenheimer <robert.e.bodenheimer@gmail.com>
2021-01-25 19:50:24 -0600

    Initial check-in

---
+++ cmake-build-debug/CMakeFiles/CS3258_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS3258_cmake.dir/main.cpp.o"
+  "CS3258_cmake"
+  "CS3258_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS3258_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
